0849d6de7cd3315730f6ba6069e287ac6d356b0c
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-18 12:34:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,13 @@
 class Backer
   attr_reader :name, :backed_projects
 
-  def initialized (name)
+  def initialize (name)
     @name = name
     @backed_projects = []
   end
 
   def back_project(project)
     @backed_projects << project
+    project.backers
   end
 end
